8bd1b3b71dbefa8b5c538eeb62637da7c09d4eea
msiarez <msiarez@terpmail.umd.edu>
2025-10-21 15:19:34 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -18,10 +18,10 @@ class Bug:
         if timestep is None:
             timestep = self.timestep
 
-        pattern = 1 << self.x
+        pattern = 1 << self.x # lights up LED at position
         self.__shifter.shiftByte(pattern)
 
-        self.x += random.choice([-1, 1])
+        self.x += random.choice([-1, 1]) # randomly moves bug left or right
 
         if self.isWrapOn:
             self.x %= 8
@@ -42,9 +42,9 @@ dataPin = 23
 latchPin = 24
 clockPin = 25
 
-switch1 = 16
-switch2 = 20
-switch3 = 21
+switch1 = 16 # start/stop switch
+switch2 = 20 # wrap switch
+switch3 = 21 # speed switch
 
 GPIO.setmode(GPIO.BCM)
 for pin in [switch1, switch2, switch3]:
@@ -52,24 +52,24 @@ for pin in [switch1, switch2, switch3]:
 
 bug = Bug()
 
-def toggle_wrap(channel):
+def toggle_wrap(channel): # flips wrap mode on and off
     bug.isWrapOn = not bug.isWrapOn
     print(f"Wrap mode toggled: {bug.isWrapOn}")
 
-GPIO.add_event_detect(switch2, GPIO.RISING, callback=toggle_wrap, bouncetime=300)
+GPIO.add_event_detect(switch2, GPIO.RISING, callback=toggle_wrap, bouncetime=300) # detects switch 2
 
 try:
     while True:
-        if GPIO.input(switch1):
+        if GPIO.input(switch1): # control the start and stop
             if not bug._running:
                 bug.start()
         else:
             if bug._running:
                 bug.stop()
 
-        current_step = bug.timestep / 3 if GPIO.input(switch3) else bug.timestep
+        current_step = bug.timestep / 3 if GPIO.input(switch3) else bug.timestep # makes bug 3x faster
 
-        bug.step(current_step)
+        bug.step(current_step) # move bug one step
 
 except KeyboardInterrupt:
     bug.stop()
