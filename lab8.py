48236b480e630783ee8a317374d27716d1c50338
msiarez <msiarez@terpmail.umd.edu>
2025-11-12 16:45:26 -0500

    Create lab8.py

---
+++ lab8.py
@@ -0,0 +1,84 @@
+import time
+import multiprocessing
+from shifter import Shifter
+
+myArray = multiprocessing.Array('i', 2)
+
+class Stepper:
+    seq = [0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001]
+    delay = 2000 
+    steps_per_degree = 1024 / 360
+
+    def __init__(self, shifter, lock, index):
+        self.s = shifter
+        self.lock = lock
+        self.index = index
+        self.angle = 0
+        self.step_state = 0
+        self.shifter_bit_start = 4 * index
+
+    def _sgn(self, x):
+        return 0 if x == 0 else int(abs(x) / x)
+
+    def _step(self, direction):
+        with self.lock:
+            self.step_state = (self.step_state + direction) % 8
+
+            myArray[self.index] &= ~(0b1111 << self.shifter_bit_start)
+            myArray[self.index] |= (Stepper.seq[self.step_state] << self.shifter_bit_start)
+
+            final = 0
+            for val in myArray:
+                final |= val
+
+            self.s.shiftByte(final)
+
+        self.angle = (self.angle + direction / Stepper.steps_per_degree) % 360
+        time.sleep(Stepper.delay / 1e6)
+
+    def _rotate(self, delta):
+        direction = self._sgn(delta)
+        steps = int(abs(delta) * Stepper.steps_per_degree)
+        for _ in range(steps):
+            self._step(direction)
+
+    def rotate(self, delta):
+        p = multiprocessing.Process(target=self._rotate, args=(delta,))
+        p.start()
+        return p
+
+    def goAngle(self, target):
+        delta = (target - self.angle + 540) % 360 - 180
+        return self.rotate(delta)
+
+    def zero(self):
+        self.angle = 0
+
+if __name__ == '__main__':
+    s = Shifter(23, 24, 25)
+    lock1 = multiprocessing.Lock()
+    lock2 = multiprocessing.Lock()
+
+    m1 = Stepper(s, lock1, 0)
+    m2 = Stepper(s, lock2, 1)
+
+    m1.zero()
+    m2.zero()
+    
+    time.sleep(.5)
+    p1 = m1.goAngle(90)
+    p2 = m2.goAngle(-90)
+    p1.join(); p2.join()
+
+    time.sleep(.5)
+    p1 = m1.goAngle(-45)
+    p1.join()
+
+    time.sleep(.5)
+    p1 = m1.goAngle(135)
+    p2 = m2.goAngle(45)
+    p1.join(); p2.join()
+
+    time.sleep(.5)
+    p1 = m1.goAngle(0)
+    p1.join()
